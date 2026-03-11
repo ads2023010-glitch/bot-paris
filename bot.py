eb0a4f420a8350c410ece820963e04a0d5375e19
ads2023010-glitch <ads2023010@gmail.com>
2026-03-11 18:08:25 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 import os
-import time
 from telegram import Bot
 
-TOKEN = os.getenv("8301048438:AAEUyBVAC3rCYACcfkUYY4GBhpp7kOSVHBs")  # Sécurise le token en variable d'environnement
-CHAT_ID = os.getenv("6898691256")  # Ton chat ID
-bot = Bot(token=TOKEN)
+TOKEN = os.getenv("TELEGRAM_TOKEN")  # récupère ton token depuis Railway
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # ton chat ID
 
-while True:
-    bot.send_message(chat_id=CHAT_ID, text="Salut ! Ceci est un message toutes les 5 minutes.")
-    time.sleep(300)  # 300 secondes = 5 minutes
+bot = Bot(token=TOKEN)
+bot.send_message(chat_id=CHAT_ID, text="Test Telegram !")
