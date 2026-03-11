3b347db1c98e09c21c1018043b7e881a7ec1f771
ads2023010-glitch <ads2023010@gmail.com>
2026-03-11 17:19:03 +0100

    bot-telegram.py

--- bot.py
+++ bot.py
@@ -1,2 +1,12 @@
+import os
+import time
 import requests
-print'"Bot lancé")
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")  # token du bot
+CHAT_ID = os.getenv("CHAT_ID")        # ton id Telegram
+
+while True:
+    message = "Salut ! C'est un message automatique 😊"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}"
+    requests.get(url)
+    time.sleep(300)  # 5 minutes = 300 secondes
